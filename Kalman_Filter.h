66b0398a04474ea072ff96af9d8e681cca64877e
Michael Mallamaci <mallamacimj@gmail.com>
2024-02-26 18:11:36 -0500

    fixed include

--- Kalman_Filter.h
+++ Kalman_Filter.h
@@ -1,9 +1,13 @@
 #ifndef KALMANFILTER_H
 #define KALMANFILTER_H
 
-//Inculded as an arduino library
+//Included as an arduino library
 
 //Include all the filters
 #include "Filters/filters.h"
+
+//Include Matrix Mult
+#include "MatrixMult/MatrixMult.h"
+#include "MatrixMult/MatrixMult.cpp"
 
 #endif
